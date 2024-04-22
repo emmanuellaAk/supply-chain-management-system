5733d8e3730343e12448787fc6be3c813b66825c
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-04-22 16:47:36 +0000

    created inventory model relationships

---
+++ resources/views/inventory.blade.php
@@ -0,0 +1,13 @@
+<x-head/>
+<x-topbar/>
+<x-sidebar/>
+<div class="content">
+    <div class="intro-y box mt-5">
+        <div class="flex flex-col sm:flex-row items-center p-5 border-b border-slate-200/60">
+            <h2 class="font-medium text-base mr-auto">
+                Products Available
+            </h2>
+        </div>
+    </div>
+</div>
+<x-script/>
