cc1aa8b6c37524f57f18d7286e8a7489de599dcf
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-04-24 22:15:35 +0000

    purchases migration,form and logic

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
 use Illuminate\Http\Request;
+use App\Models\PurchaseOrder;
 
 class PurchaseOrderController extends Controller
 {
+    public function index()
+    {
+         return view('all-purchases');
+    }
+
     public function create()
     {
-        return view('purchase-order');
+        $products = Inventory::all();
+        return view('purchase-order', ['products' => $products]);
+    }
+
+    public function store(Request $request)
+    {
+        $attributes = request()->validate([
+        'product'=>'required',
+        'quantity'=>'required',
+       ]);
+
+       PurchaseOrder::create([
+        'product_id'=>$request->product,
+        'quantity'=>$request->quantity,
+        'order_status' =>'pending'
+       ]);
     }
 }
